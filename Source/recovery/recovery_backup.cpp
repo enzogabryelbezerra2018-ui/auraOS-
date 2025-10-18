8cd08efd4810a627a17ebbd09d501d1c38173ac9
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 09:22:22 -0300

    Create recovery_backup.cpp

---
+++ Source/recovery/recovery_backup.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void create_backup() {
+    std::cout << "[Recovery] Criando backup completo...\n";
+    std::cout << "[Recovery] Backup salvo em /sdcard/AuraOS_Backup.aura\n";
+}
