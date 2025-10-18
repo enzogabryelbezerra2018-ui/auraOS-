9f8ed19d96d49809dd47f281c9f702d27ba859a6
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 12:46:04 -0300

    Create boot_config.h

---
+++ Source/bootlooader/boot_config.h
@@ -0,0 +1,20 @@
+#ifndef BOOT_CONFIG_H
+#define BOOT_CONFIG_H
+
+#define MENU_ITEM_COUNT 4
+#define MENU_DELAY_MS 100
+
+// Cores RGB para botões e fundo
+#define COLOR_BG_R 0
+#define COLOR_BG_G 0
+#define COLOR_BG_B 0
+
+#define COLOR_HIGHLIGHT_R 0
+#define COLOR_HIGHLIGHT_G 255
+#define COLOR_HIGHLIGHT_B 255
+
+#define COLOR_TEXT_R 255
+#define COLOR_TEXT_G 255
+#define COLOR_TEXT_B 255
+
+#endif
