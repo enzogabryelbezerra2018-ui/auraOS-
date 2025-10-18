fe81a9583243ef6a31bfa6207bd8e0058e5d2151
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 09:45:50 -0300

    Create alert_network.h

---
+++ Source/alert_system/alert_network.h
@@ -0,0 +1,11 @@
+#ifndef ALERT_NETWORK_H
+#define ALERT_NETWORK_H
+
+#include "alert_system.h"
+
+class AlertNetwork {
+public:
+    Alert get_remote_alert(); // Simula alerta via rede
+};
+
+#endif
