cdc5e908f0c0e1d6ec03c858a34e44b2a8ad02b5
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 12:52:15 -0300

    Create boot_animations.h

---
+++ Source/bootlooader/boot_animations.h
@@ -0,0 +1,10 @@
+#ifndef BOOT_ANIMATIONS_H
+#define BOOT_ANIMATIONS_H
+
+#include "boot_palette.h"
+
+void fade_in_screen(int duration_ms);
+void fade_out_screen(int duration_ms);
+void animate_arrow(int x, int y, int repeat);
+
+#endif
