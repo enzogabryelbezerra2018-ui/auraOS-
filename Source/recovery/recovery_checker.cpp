ed951c2a288df770b2380f9a274e523e0fc08536
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 09:23:14 -0300

    Create recovery_checker.cpp

---
+++ Source/recovery/recovery_checker.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void check_system_integrity() {
+    std::cout << "[Recovery] Verificando integridade do sistema...\n";
+    std::cout << "[Recovery] Nenhum erro encontrado.\n";
+}
