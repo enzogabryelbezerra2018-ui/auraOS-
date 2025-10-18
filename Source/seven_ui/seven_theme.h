38f4a30e9aac14621757314aa6afcdb00bdec065
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-17 22:12:08 -0300

    Create seven_theme.h

---
+++ Source/seven_ui/seven_theme.h
@@ -0,0 +1,13 @@
+#ifndef SEVEN_THEME_H
+#define SEVEN_THEME_H
+
+#include <stdint.h>
+
+// Paleta de cores base, pode ser manipulada pelo color_logic.cpp
+#define COLOR_BG        0xFF101010
+#define COLOR_PRIMARY   0xFF1E90FF
+#define COLOR_SECONDARY 0xFF00FFFF
+#define COLOR_TEXT      0xFFFFFFFF
+#define COLOR_HIGHLIGHT 0xFFFFFF00
+
+#endif
