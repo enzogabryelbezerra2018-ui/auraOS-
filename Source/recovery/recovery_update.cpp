6f78e5901087ff1638ffd4c0136fb11d135ec548
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 09:22:51 -0300

    Create recovery_update.cpp

---
+++ Source/recovery/recovery_update.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void apply_update_from_file(const std::string &path) {
+    std::cout << "[Recovery] Verificando arquivo: " << path << "\n";
+    std::cout << "[Recovery] Extraindo e aplicando atualização...\n";
+    std::cout << "[Recovery] Sistema atualizado!\n";
+}
