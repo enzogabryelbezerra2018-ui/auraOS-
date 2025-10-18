06d5a7a223e522baf1b79e0e51fa4bf993b36b06
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 13:04:19 -0300

    Create init.c

---
+++ Source/no_main/init.c
@@ -0,0 +1,8 @@
+#include "init.h"
+#include <stdio.h>
+
+void init_system() {
+    // Inicialização mínima de módulos essenciais
+    printf("[NoMain] Inicializando módulos críticos...\n");
+    // Aqui você poderia inicializar memória, periféricos ou logs
+}
