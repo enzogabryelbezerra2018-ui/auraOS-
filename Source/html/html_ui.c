74c44e5e4b0b7660950d39dc4841ccaf41e0ef7b
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:06:03 -0300

    Update html_ui.c

--- Source/html/html_ui.c
+++ Source/html/html_ui.c
@@ -5,63 +5,60 @@
 
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 240
-#define CHAR_WIDTH    8
-#define CHAR_HEIGHT   16
 
-static int scroll_offset = 0;
+static int scroll_x = 0;
+static int scroll_y = 0;
+static int char_width = 8;
+static int char_height = 16;
 
-// Desenha buffer de caracteres coloridos na tela
+void html_set_zoom(int zoom_factor) {
+    char_width = 8 * zoom_factor;
+    char_height = 16 * zoom_factor;
+}
+
 void html_draw_colored(html_char_t* buffer, size_t len) {
-    int cur_x = 0;
-    int cur_y = -scroll_offset;
+    int cur_x = -scroll_x;
+    int cur_y = -scroll_y;
 
     for (size_t i = 0; i < len; ++i) {
         if (buffer[i].ch == '\n') {
-            cur_x = 0;
-            cur_y += CHAR_HEIGHT;
+            cur_x = -scroll_x;
+            cur_y += char_height;
             continue;
         }
 
-        if (cur_y >= 0 && cur_y < SCREEN_HEIGHT) {
+        if (cur_x >= -char_width && cur_x < SCREEN_WIDTH && cur_y >= -char_height && cur_y < SCREEN_HEIGHT) {
             seven_ui_draw_char(buffer[i].ch, cur_x, cur_y, buffer[i].color);
         }
 
-        cur_x += CHAR_WIDTH;
-        if (cur_x + CHAR_WIDTH > SCREEN_WIDTH) {
-            cur_x = 0;
-            cur_y += CHAR_HEIGHT;
+        cur_x += char_width;
+        if (cur_x + char_width > SCREEN_WIDTH) {
+            cur_x = -scroll_x;
+            cur_y += char_height;
         }
     }
 }
 
-// Scroll vertical
-void html_scroll_up(int pixels) {
-    scroll_offset -= pixels;
-    if (scroll_offset < 0) scroll_offset = 0;
+void html_scroll(int dx, int dy) {
+    scroll_x += dx;
+    scroll_y += dy;
+    if (scroll_x < 0) scroll_x = 0;
+    if (scroll_y < 0) scroll_y = 0;
 }
 
-void html_scroll_down(int pixels) {
-    scroll_offset += pixels;
-}
-
-// Atualiza cores em tempo real
-void html_refresh_colors(html_char_t* buffer, size_t len) {
-    for (size_t i = 0; i < len; ++i) {
-        buffer[i].color = colors[rand() % NUM_COLORS];
-    }
-}
-
-// Função principal: abre, atualiza cores e desenha HTML
-void html_open_and_display(const char* path) {
+// Função principal
+void html_open_and_display(const char* path, int random_per_line) {
     static html_char_t buffer[MAX_FILE_SIZE] = {0};
     static size_t len = 0;
+    static size_t line_cache[MAX_FILE_SIZE / 16] = {0}; // cache por linha
     static int first_load = 1;
 
     if (first_load) {
         len = html_load_file_colored(path, buffer, MAX_FILE_SIZE);
+        memset(line_cache, 0, sizeof(line_cache));
         first_load = 0;
     } else {
-        html_refresh_colors(buffer, len); // muda cores dinamicamente
+        html_refresh_colors_cached(buffer, len, line_cache, random_per_line);
     }
 
     html_draw_colored(buffer, len);
