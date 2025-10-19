89aa401a046cf8ba3486c8eb4b4a1c1ef639cd69
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:24:16 -0300

    Update internet_ui.c

--- Source/Internet/src/ui/internet_ui.c
+++ Source/Internet/src/ui/internet_ui.c
@@ -1,14 +1,41 @@
 #include "internet_ui.h"
 #include "seven_ui.h"
+#include <vector>
+#include "html_logic.h"
 
-// Desenha barra de endereço, botões, conteúdo
+static int scroll_y = 0;
+#define CHAR_WIDTH 8
+#define CHAR_HEIGHT 16
+
+void html_draw() {
+    int cur_y = -scroll_y;
+    const std::vector<HtmlChar>& buf = get_html_buffer();
+
+    int cur_x = 0;
+    for(size_t i=0;i<buf.size();i++){
+        if(buf[i].ch=='\n'){
+            cur_x = 0;
+            cur_y += CHAR_HEIGHT;
+            continue;
+        }
+        if(cur_y>=0 && cur_y<240){ // dentro da tela
+            seven_ui_draw_char(buf[i].ch, cur_x, cur_y, buf[i].color);
+        }
+        cur_x+=CHAR_WIDTH;
+        if(cur_x+CHAR_WIDTH>320){
+            cur_x=0;
+            cur_y+=CHAR_HEIGHT;
+        }
+    }
+}
+
+void html_scroll(int dy){
+    scroll_y+=dy;
+    if(scroll_y<0) scroll_y=0;
+}
+
 void draw_browser_ui() {
     draw_address_bar();
     draw_navigation_buttons();
-    draw_html_content();
-}
-
-// Conteúdo HTML: receber de HTMLInterpreter
-void draw_html_content() {
-    // TODO: implementar render baseado em parse_html()
+    html_draw();
 }
