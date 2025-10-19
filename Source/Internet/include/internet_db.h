9e0d20ef7942b0f79b16502724174cca9c90248c
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:26:41 -0300

    Update internet_db.h

--- Source/Internet/include/internet_db.h
+++ Source/Internet/include/internet_db.h
@@ -1,8 +1,10 @@
+// include/internet_db.h
 #ifndef INTERNET_DB_H
 #define INTERNET_DB_H
 
 #include <string>
 
+// Salva página no cache / histórico
 void cache_page(const std::string& url, const std::string& html_data);
 
-#endif
+#endif // INTERNET_DB_H
