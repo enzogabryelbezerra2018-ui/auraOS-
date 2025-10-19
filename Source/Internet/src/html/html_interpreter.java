ae7731d1cd14885dd69fcb6fbd3ed8acecb5dbe8
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:12:45 -0300

    Create html_interpreter.java

---
+++ Source/Internet/src/html/html_interpreter.java
@@ -0,0 +1,6 @@
+public class HTMLInterpreter {
+    public static LineData[] parseHTML(String html) {
+        // Converte HTML em linhas e cores para enviar para C UI
+        return new LineData[]{};
+    }
+}
