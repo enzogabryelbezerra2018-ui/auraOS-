79a7acfd50a9c331fb0017a3b57a0ae88c6501a3
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:21:11 -0300

    Update html_interpreter.java

--- Source/Internet/src/html/html_interpreter.java
+++ Source/Internet/src/html/html_interpreter.java
@@ -1,12 +1,41 @@
+// html/html_interpreter.java
 public class HTMLInterpreter {
-    public static LineData[] parseHTML(String html) {
-        // Retorna linhas e cores para UI
-        // TODO: implementar parser simples que converte <b>, <i>, <a>
-        return new LineData[0];
-    }
-}
 
-class LineData {
-    public String text;
-    public int color;
+    // Estrutura de linha com texto e cor
+    public static class LineData {
+        public String text;
+        public int color;
+        public String link; // se houver <a href>
+        public LineData(String t, int c, String l) { text=t; color=c; link=l; }
+    }
+
+    public static LineData[] parseHTML(String html) {
+        java.util.List<LineData> lines = new java.util.ArrayList<>();
+        int colorDefault = 0xFFFFFF;
+        int colorH1 = 0xFF0000;
+        int colorBold = 0xFFFF00;
+        int colorItalic = 0x00FF00;
+        int colorLink = 0x00BFFF;
+
+        String[] rawLines = html.split("\n");
+        for(String l : rawLines) {
+            String text = l.replaceAll("<[^>]*>", ""); // remove tags
+            int color = colorDefault;
+            String link = null;
+
+            if (l.contains("<h1>")) color = colorH1;
+            else if (l.contains("<b>")) color = colorBold;
+            else if (l.contains("<i>")) color = colorItalic;
+            else if (l.contains("<a href=\"")) {
+                color = colorLink;
+                int start = l.indexOf("<a href=\"")+9;
+                int end = l.indexOf("\"", start);
+                if(end>start) link = l.substring(start,end);
+            }
+
+            lines.add(new LineData(text, color, link));
+        }
+
+        return lines.toArray(new LineData[0]);
+    }
 }
