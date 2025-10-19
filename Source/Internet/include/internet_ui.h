cab4f2e1b1cd56225386b6419c2906d8efbb24c6
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:26:20 -0300

    Update internet_ui.h

--- Source/Internet/include/internet_ui.h
+++ Source/Internet/include/internet_ui.h
@@ -1,7 +1,14 @@
+// include/internet_ui.h
 #ifndef INTERNET_UI_H
 #define INTERNET_UI_H
 
+// Desenha toda a interface do navegador: barra de endereço, botões, conteúdo
 void draw_browser_ui();
+
+// Desenha apenas o conteúdo HTML (recebido do html_logic)
 void draw_html_content();
 
-#endif
+// Scroll vertical do conteúdo
+void html_scroll(int dy);
+
+#endif // INTERNET_UI_H
