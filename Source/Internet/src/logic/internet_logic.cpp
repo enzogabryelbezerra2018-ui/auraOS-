44b9a1dee3e1e60de0d76455f7530d6570e4e053
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:15:34 -0300

    Update internet_logic.cpp

--- Source/Internet/src/logic/internet_logic.cpp
+++ Source/Internet/src/logic/internet_logic.cpp
@@ -1,10 +1,15 @@
 #include "internet_logic.h"
 #include "internet_db.h"
 #include "../network/http_client.h"
+#include "../html/html_interpreter.h"
 #include <string>
 
 void navigate_to(const std::string& url) {
-    std::string html_data = http_download(url);
-    cache_page(url, html_data);             // salva no banco
-    html_interpret(html_data);              // envia para interpretador Java
+    std::string html_data = http_download(url);  // baixa HTML
+    cache_page(url, html_data);                   // salva cache
+    render_page(html_data);                       // envia para interpretador
+}
+
+void render_page(const std::string& html_data) {
+    parse_html(html_data);  // chama Java ou engine embutida
 }
