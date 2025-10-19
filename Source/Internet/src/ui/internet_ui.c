7d3fe7d7d98d700e996f69fc14f6b5b5563a06f6
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:19:49 -0300

    Update internet_ui.c

--- Source/Internet/src/ui/internet_ui.c
+++ Source/Internet/src/ui/internet_ui.c
@@ -1,8 +1,14 @@
 #include "internet_ui.h"
 #include "seven_ui.h"
 
+// Desenha barra de endereço, botões, conteúdo
 void draw_browser_ui() {
     draw_address_bar();
     draw_navigation_buttons();
-    draw_html_content(); // chama interpretador Java para retornar pixels/texto
+    draw_html_content();
+}
+
+// Conteúdo HTML: receber de HTMLInterpreter
+void draw_html_content() {
+    // TODO: implementar render baseado em parse_html()
 }
