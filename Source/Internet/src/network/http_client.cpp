2d523cf99235e0973cccca862f155dd67c1f9943
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:13:26 -0300

    Create http_client.cpp

---
+++ Source/Internet/src/network/http_client.cpp
@@ -0,0 +1,7 @@
+#include "http_client.h"
+#include <string>
+
+std::string http_download(const std::string& url) {
+    // implementação simples do download
+    return "<html><body>Página de teste</body></html>";
+}
