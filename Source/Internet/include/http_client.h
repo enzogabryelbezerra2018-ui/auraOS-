339e5e73d2dce490e407da64c913fc0b8f89cb1f
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:27:08 -0300

    Update http_client.h

--- Source/Internet/include/http_client.h
+++ Source/Internet/include/http_client.h
@@ -1,8 +1,10 @@
+// include/http_client.h
 #ifndef HTTP_CLIENT_H
 #define HTTP_CLIENT_H
 
 #include <string>
 
+// Baixa página HTML de uma URL (HTTP/HTTPS)
 std::string http_download(const std::string& url);
 
-#endif
+#endif // HTTP_CLIENT_H
