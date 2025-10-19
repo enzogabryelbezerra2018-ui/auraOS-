92d5431bbcfd2c4b134945d1daa08d5acb418d3e
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-18 22:12:15 -0300

    Create internet_db.cpp

---
+++ Source/Internet/src/db/internet_db.cpp
@@ -0,0 +1,14 @@
+#include "internet_db.h"
+#include <vector>
+#include <string>
+
+struct PageRecord {
+    std::string url;
+    std::string html_data;
+};
+
+std::vector<PageRecord> history;
+
+void cache_page(const std::string& url, const std::string& html_data) {
+    history.push_back({url, html_data});
+}
